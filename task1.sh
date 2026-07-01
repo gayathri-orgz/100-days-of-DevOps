9bc1d5741948d2180593057bfb67e8c02fe53cac
its-gayathri <163381155+gayathri-projects@users.noreply.github.com>
2026-07-01 09:48:24 +0530

    Delete task1.sh

--- task1.sh
+++
@@ -1,8 +0,0 @@
-# logging into the app server 1 (stapp01)
-> ssh tony@stapp01  
-
-# Create a user with non interactive shell 
-> sudo useradd -s /sbin/nologin javed  
-
-# List of the users 
-> cat /etc/passwd 
