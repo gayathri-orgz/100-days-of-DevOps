6b73e35cfe61bf0a7360b44b0963186ba3cc51c3
its-gayathri <163381155+gayathri-projects@users.noreply.github.com>
2026-07-01 09:00:43 +0530

    Add user management script with login and user listing
    This script facilitates user management tasks on the app server, including user creation and listing.

---
+++ Assignment-1/task-1.sh
@@ -0,0 +1,8 @@
+# logging into the app server 1 (stapp01)
+> ssh tony@stapp01  
+
+# Create a user with non interactive shell 
+> sudo useradd -s /sbin/nologin javed  
+
+# List of the users 
+> cat /etc/passwd 
